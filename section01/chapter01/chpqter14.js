159fbdf53cf866673511a2a31f1a5d3c9ade2c59
johanness917 <jhhan917@gmail.com>
2026-02-03 17:57:17 +0900

    [생성] 자바스크립트 객체 생성, 조회, 추가, 삭제 및 동적기능테스트

--- section01/chapter01/chpqter14.js
+++ section01/chapter01/chpqter14.js
@@ -1,55 +1,42 @@
-// 1. 객체 생성 
-let obj1 = {};          // 객체 리터럴 (대부분 사용) 
-let obj2 = new Object(); // 객체 생성자 
-console.log(typeof obj1);
-console.log(typeof obj2);
+// 1. 객체 생성
+let obj1 = new Object(); // 객체 생성자
+let obj2 = {};          // 객체 리터럴 (대부분 사용)
 
-// 2. 객체 프로퍼티 (객체 속성) 
+// 2. 객체 프로퍼티 (객체 속성)
 let person = {
   name: "홍길동",
   age: 30,
   hobby: "축구",
   job: "Developer",
-  extra: {},   //객체도 들어올수 있음 
-  extra2: function () { },  //함수도 들어올수 있음 
-  "like cat": true,  //한칸띄는 변수가 있으면 “”  
+  extra: {},   //객체도 들어올수 있음
+  extra2: function () { },  //함수도 들어올수 있음
+  "like cat": true,  //한칸띄는 변수가 있으면 “” 
 };
 
-console.log(person);
-console.log(person.name);
-console.log(person["name"]);
-person.extra2 = (index) => console.log(index);
-console.log(person.extra2("화살표함수"));
-person.extra = {
-  width: 10,
-  height: 20
-};
-console.log(person.extra);
-console.log(person);
-
-// 3. 객체 프로퍼티를 다루는 방법 
-// 3.1 특정 프로퍼티에 접근 (점 표기법, 대괄호 표기법) 
+// 3. 객체 프로퍼티를 다루는 방법
+// 3.1 특정 프로퍼티에 접근 (점 표기법, 대괄호 표기법)
 let name = person.name;
-let age = person["age2"];  //존재하지 않는 변수가 나오면 undefind 저장한다. 
+let age = person["age2"];  //존재하지 않는 변수가 나오면 undefind 저장한다.
 
 let property = "hobby";
 let hobby = person[property];
 
-// 3.2 새로운 프로퍼티를 추가하는 방법 
+// 3.2 새로운 프로퍼티를 추가하는 방법
 person.job = "fe developer";
 person["favoriteFood"] = "떡볶이";
 console.log(person);
 
-// 3.3 프로퍼티를 수정하는 방법 
+// 3.3 프로퍼티를 수정하는 방법
 person.job = "educator";
 person["favoriteFood"] = "초콜릿";
 console.log(person);
-// 3.4 프로퍼티를 삭제하는 방법 
+
+// 3.4 프로퍼티를 삭제하는 방법
 delete person.job;
 delete person["favoriteFood"];
 console.log(person);
-// 3.5 프로퍼티의 존재 유무를 확인하는 방법 (in 연산자) 
+
+// 3.5 프로퍼티의 존재 유무를 확인하는 방법 (in 연산자)
 let result1 = "name" in person;
 let result2 = "cat" in person;
 console.log(result2);
-
