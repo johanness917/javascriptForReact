76717315074a97dc2ed55554540a1ef83eddd0bf
johanness917 <jhhan917@gmail.com>
2026-02-04 11:29:39 +0900

    [생성] 클래스 구조에서 멤버변수 추출하기

---
+++ section01/chapter01/chapter19.js
@@ -0,0 +1,20 @@
+// 클래스속에 있는 멤버변수를 반복문을 이용해서 출력
+const person = {
+  name: "김동진",
+  age: 25,
+  tall: 179
+};
+
+const personKeys = Object.keys(person);
+// const personKeys = ['name', 'age', 'tall']
+console.log(personKeys);
+
+for (let index = 0; index < personKeys.length; index++) {
+  const key = personKeys[index];
+  console.log(`${key} = {person[key]}`);
+}
+
+const personValues = Object.values(person); //객체의 value값을 배열로가져온다. 
+for (let i = 0; i < personValues.length; i++) {
+  console.log(personValues[i]);
+} 
