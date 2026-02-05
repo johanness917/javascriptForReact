8acd939cf5a5248669c6db2fefaf1c398437ba69
johanness917 <jhhan917@gmail.com>
2026-02-05 09:29:57 +0900

    [추가] 구조분해할당 추가내용

--- section01/chapter01/chapter25.js
+++ section01/chapter01/chapter25.js
@@ -1,4 +1,4 @@
-//구조분해할당 (배열, 분해, 변수할당)
+//1. 구조분해할당 (배열, 분해, 변수할당)
 let arr = [1, 2];
 let one = arr[0];
 let two = arr[1];
@@ -10,7 +10,7 @@ console.log(_two);
 console.log(_three);
 console.log(_four);
 
-//구조분해할당 (배열, 분해, 변수할당)
+//2. 구조분해할당 (배열, 분해, 변수할당)
 let person = {
   name: "홍길동",
   age: 27,
@@ -31,3 +31,19 @@ let _myAge = person.age;
 let _myName = person.name;
 
 console.log(myAge, myName, myHobby, extra);
+
+//3. 객체 구조 분해 할당을 이용해서 함수의 매개변수를 받는 방법 
+//각 멤버변수를 변수로 저장하고, 출력하고 싶다.
+let printPerson = ({ name, age, hobby, extra = "hello" }) => {
+  console.log(`name = ${name}`);
+  console.log(`age = ${age}`);
+  console.log(`hobby = ${bobby}`);
+  console.log(`extra = ${extra}`);
+}
+printPerson(person);
+
+const func = ({ name, age, hobby, extra }) => {
+  console.log(name, age, hobby, extra);
+};
+
+func(person); 
